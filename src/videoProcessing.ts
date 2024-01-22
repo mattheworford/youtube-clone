13d25e8ba4a03ffafa6403694c7c6dbba84c1b36
Matthew Orford <mattorford9@gmail.com>
2024-01-21 23:55:59 -0500

    reformat code

---
+++ src/videoProcessing.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
+import fs from 'fs';
+
+
+interface VideoProcessingRequestBody {
+    inputFilePath: string;
+    outputFilePath: string;
+}
+
+export function processVideo(req: express.Request<{}, {}, VideoProcessingRequestBody>, res: express.Response) {
+    const { inputFilePath, outputFilePath } = req.body;
+
+    if (!inputFilePath) {
+        return res.status(400).send('Bad Request: Missing `inputFilePath`.');
+    }
+
+    if (!outputFilePath) {
+        return res.status(400).send('Bad Request: Missing `outputFilePath`.');
+    }
+
+    if (!fs.existsSync(inputFilePath)) {
+        return res.status(400).send(`Bad Request: File not found at ${inputFilePath}.`);
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions('-vf', 'scale=-1:360')
+        .on('end', function () {
+            const successMessage = `Successfully processed ${inputFilePath}`;
+            console.log(successMessage);
+            res.status(200).send(successMessage);
+        })
+        .on('error', function (err: any) {
+            const errorMessage = `An error occurred: ${err.message}`;
+            console.error(errorMessage);
+            res.status(500).send(errorMessage);
+        })
+        .run();
+};
