62a666a4b6df8c53a2fc0e4ba5b1c0a018490836
Matthew Orford <mattorford9@gmail.com>
2024-01-23 18:57:34 -0500

    Revert "introduce express validator and refactor for usage"
    This reverts commit df948bcebbf2f204383203ea80b1743bb4a9826f.

--- src/videoProcessing.ts
+++ src/videoProcessing.ts
@@ -1,27 +1,47 @@
+import express from 'express';
 import ffmpeg from 'fluent-ffmpeg';
 import fs from 'fs';
-import { Error } from './model/error';
 
-export function scaleVideo(
-  inputFilePath: string,
-  outputFilePath: string,
-  height: number = 360
-): Promise<string> {
-  return new Promise((resolve, reject) => {
-    if (!fs.existsSync(inputFilePath)) {
-      reject(`File not found at ${inputFilePath}.`);
-    }
+interface VideoProcessingRequestBody {
+  inputFilePath: string;
+  outputFilePath: string;
+}
 
-    ffmpeg(inputFilePath)
-      .outputOptions('-vf', `scale=-1:${height.toString}`)
-      .on('end', function () {
-        resolve(
-          `Successfully scaled ${inputFilePath} to a height of ${height.toString} pixels.`
-        );
-      })
-      .on('error', function (err: Error) {
-        reject(`An error occurred: ${err.message}.`);
-      })
-      .save(outputFilePath);
-  });
+interface Error {
+  message: string;
+}
+
+export function processVideo(
+  req: express.Request<object, object, VideoProcessingRequestBody>,
+  res: express.Response
+) {
+  const { inputFilePath, outputFilePath } = req.body;
+
+  if (!inputFilePath) {
+    return res.status(400).send('Bad Request: Missing `inputFilePath`.');
+  }
+
+  if (!outputFilePath) {
+    return res.status(400).send('Bad Request: Missing `outputFilePath`.');
+  }
+
+  if (!fs.existsSync(inputFilePath)) {
+    return res
+      .status(400)
+      .send(`Bad Request: File not found at ${inputFilePath}.`);
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions('-vf', 'scale=-1:360')
+    .on('end', function () {
+      const successMessage = `Successfully processed ${inputFilePath}`;
+      console.log(successMessage);
+      res.status(200).send(successMessage);
+    })
+    .on('error', function (err: Error) {
+      const errorMessage = `An error occurred: ${err.message}`;
+      console.error(errorMessage);
+      res.status(500).send(errorMessage);
+    })
+    .run();
 }
