17d2a481bfb1fb4aa9243b812f121a2010ea8fa4
Matthew Orford <mattorford9@gmail.com>
2024-01-23 18:58:06 -0500

    Add express-validator for input validation and update videoProcessing.ts to use scaleVideo function

--- src/videoProcessing.ts
+++ src/videoProcessing.ts
@@ -1,47 +1,27 @@
-import express from 'express';
 import ffmpeg from 'fluent-ffmpeg';
 import fs from 'fs';
+import { Error } from './model/error';
 
-interface VideoProcessingRequestBody {
-  inputFilePath: string;
-  outputFilePath: string;
-}
+export function scaleVideo(
+  inputFilePath: string,
+  outputFilePath: string,
+  height: number = 360
+): Promise<string> {
+  return new Promise((resolve, reject) => {
+    if (!fs.existsSync(inputFilePath)) {
+      reject(`File not found at ${inputFilePath}.`);
+    }
 
-interface Error {
-  message: string;
-}
-
-export function processVideo(
-  req: express.Request<object, object, VideoProcessingRequestBody>,
-  res: express.Response
-) {
-  const { inputFilePath, outputFilePath } = req.body;
-
-  if (!inputFilePath) {
-    return res.status(400).send('Bad Request: Missing `inputFilePath`.');
-  }
-
-  if (!outputFilePath) {
-    return res.status(400).send('Bad Request: Missing `outputFilePath`.');
-  }
-
-  if (!fs.existsSync(inputFilePath)) {
-    return res
-      .status(400)
-      .send(`Bad Request: File not found at ${inputFilePath}.`);
-  }
-
-  ffmpeg(inputFilePath)
-    .outputOptions('-vf', 'scale=-1:360')
-    .on('end', function () {
-      const successMessage = `Successfully processed ${inputFilePath}`;
-      console.log(successMessage);
-      res.status(200).send(successMessage);
-    })
-    .on('error', function (err: Error) {
-      const errorMessage = `An error occurred: ${err.message}`;
-      console.error(errorMessage);
-      res.status(500).send(errorMessage);
-    })
-    .run();
+    ffmpeg(inputFilePath)
+      .outputOptions('-vf', `scale=-1:${height.toString}`)
+      .on('end', function () {
+        resolve(
+          `Successfully scaled ${inputFilePath} to a height of ${height.toString} pixels.`
+        );
+      })
+      .on('error', function (err: Error) {
+        reject(`An error occurred: ${err.message}.`);
+      })
+      .save(outputFilePath);
+  });
 }
