60d470d441d47f01155a62323ad48a4008610776
Matthew Orford <mattorford9@gmail.com>
2024-01-28 17:44:59 -0500

    add final test

--- tests/videoProcessing.test.ts
+++ tests/videoProcessing.test.ts
@@ -4,22 +4,11 @@ import { jest, describe, expect, it } from '@jest/globals';
 import ffmpeg from 'fluent-ffmpeg';
 
 jest.mock('fluent-ffmpeg', () => {
-  return jest.fn(function (this: jest.Mocked<ffmpeg.FfmpegCommand>) {
-    return {
-      outputOptions: jest.fn().mockReturnThis(),
-      save: jest.fn().mockReturnThis(),
-      on: jest.fn(function (
-        this: jest.Mocked<ffmpeg.FfmpegCommand>,
-        event: unknown,
-        callback: unknown
-      ) {
-        if (event === 'end') {
-          (callback as () => void)();
-        }
-        return this;
-      })
-    };
-  });
+  return jest.fn().mockImplementation(() => ({
+    outputOptions: jest.fn().mockReturnThis(),
+    save: jest.fn().mockReturnThis(),
+    on: jest.fn().mockReturnThis()
+  }));
 });
 
 jest.mock('fs', () => ({
@@ -27,6 +16,16 @@ jest.mock('fs', () => ({
 }));
 
 describe('scaleVideo', () => {
+  let mockFfmpegCommand: jest.Mocked<ffmpeg.FfmpegCommand>;
+
+  beforeEach(() => {
+    (fs.existsSync as jest.Mock).mockReturnValue(true);
+    mockFfmpegCommand = ffmpeg() as jest.Mocked<ffmpeg.FfmpegCommand>;
+    (ffmpeg as jest.MockedFunction<typeof ffmpeg>).mockReturnValue(
+      mockFfmpegCommand
+    );
+  });
+
   it('rejects if the input file does not exist', async () => {
     (fs.existsSync as jest.Mock).mockReturnValue(false);
 
@@ -36,10 +35,38 @@ describe('scaleVideo', () => {
   });
 
   it('resolves with a success message if the video is processed successfully', async () => {
-    (fs.existsSync as jest.Mock).mockReturnValue(true);
+    jest.spyOn(mockFfmpegCommand, 'on').mockImplementation(function (
+      this: jest.Mocked<ffmpeg.FfmpegCommand>,
+      event: unknown,
+      callback: unknown
+    ) {
+      if (event === 'end') {
+        (callback as () => void)();
+      }
+      return this;
+    });
 
     await expect(scaleVideo('input.mp4', 'output.mp4')).resolves.toEqual(
       'Successfully scaled input.mp4 to a height of 360 pixels.'
+    );
+  });
+
+  it('rejects with an error message if the video processing fails', async () => {
+    jest.spyOn(mockFfmpegCommand, 'on').mockImplementation(function (
+      this: jest.Mocked<ffmpeg.FfmpegCommand>,
+      event: unknown,
+      callback: unknown
+    ) {
+      if (event === 'error') {
+        (callback as (err: Error) => void)(
+          new Error('Video processing failed.')
+        );
+      }
+      return this;
+    });
+
+    await expect(scaleVideo('input.mp4', 'output.mp4')).rejects.toEqual(
+      'An error occurred: Video processing failed.'
     );
   });
 });
